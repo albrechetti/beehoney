d2cbbb642d2fb92aec1e6ba91e723685e2857f7b
albrechetti.06 <luan@scorefy.com>
2021-11-27 02:29:45 -0300

    onLoad base

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,30 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/widgets.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(GameWidget(game: BeeHoney()));
 }
 
-class BeeHoney extends FlameGame {}
+class BeeHoney extends FlameGame {
+  SpriteComponent bg = SpriteComponent();
+  SpriteComponent bee = SpriteComponent();
+
+  @override
+  Future<void>? onLoad() async {
+    bg
+      ..sprite = await Sprite.load('bg.png')
+      ..size.x = 500
+      ..size.y = 900
+      ..position = Vector2(0, 0);
+    add(bg);
+
+    bee
+      ..sprite = await Sprite.load('bee1.png')
+      ..size = Vector2.all(50)
+      ..position = Vector2(250, 800);
+    add(bee);
+    return super.onLoad();
+  }
+}
